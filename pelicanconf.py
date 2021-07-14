16dab8d6ff1cb245c2b4bddcf1e540a4d9ff2f0f
hankliao87 <hankliao87@gmail.com>
2021-07-15 03:31:17 +0800

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,31 +6,25 @@ AUTHOR = 'hankliao87'
 SITENAME = u'Hank Liao\'s Blog'
 # SITESUBTITLE = 'A personal blog.'
 SITEURL = ''
-THEME = 'theme'
-TIMEZONE = 'Asia/Taipei'
-DEFAULT_LANG = 'en'
-PATH = 'content'
 LOGO_IMG = 'images/logo.png'
 REPO_NAME = 'hankliao87/blog'
+THEME = 'theme'
+PATH = 'content'
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
-GITHUB_URL = 'https://github.com/hankliao87/'
-# TWITTER_URL = '/'
-# FACEBOOK_URL = '/'
-
-# Blogroll
-LINKS = ()
-
-# Social widget
-SOCIAL = ()
+MENUITEMS = (
+  ('Home','/'),
+  ('About','/about.html'),
+  ('Blog','/blog/'),
+)
 
 DEFAULT_PAGINATION = 5
+# SUMMARY_MAX_LENGTH = 20
+
+DEFAULT_LANG = 'en'
+TIMEZONE = 'Asia/Taipei'
+DATE_FORMATS = {
+    'en': ('%Y/%m/%d'),
+}
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
@@ -44,7 +38,6 @@ STATIC_PATHS = ['images']
 DIRECT_TEMPLATES = ['index', 'tags', 'categories', 'authors', 'archives', 'search']
 
 DISPLAY_CATEGORIES_ON_MENU = False
-DISPLAY_PAGES_ON_MENU = False
 USE_FOLDER_AS_CATEGORY = False
 
 ARTICLE_PATHS = ['articles']
@@ -53,6 +46,7 @@ ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{slug}/'
 ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{slug}/index.html'
 ARTICLE_ORDER_BY = 'reversed-date'
 
+DISPLAY_PAGES_ON_MENU = False
 PAGE_PATHS = ['pages']
 PAGE_EXCLUDES = []
 PAGE_URL = 'pages/{slug}/'
@@ -61,18 +55,34 @@ PAGE_SAVE_AS = 'pages/{slug}/index.html'
 DRAFT_URL = 'posts/draft/{slug}/'
 DRAFT_SAVE_AS = 'posts/draft/{slug}/index.html'
 
-MENUITEMS = (
-  ('Home','/'),
-  ('About','/about.html'),
-#   ('Contact','/contact/'),
-  ('Blog','/blog/'),
-)
+################################
+# Feed
+################################
 
-# SUMMARY_MAX_LENGTH = 20
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
 
-DATE_FORMATS = {
-    'en': ('%Y/%m/%d'),
-}
+################################
+# Links
+################################
+
+GITHUB_URL = 'https://github.com/hankliao87/'
+# TWITTER_URL = '/'
+# FACEBOOK_URL = '/'
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = ()
+
+################################
+# PLUGIN
+################################
 
 PLUGIN_PATHS = ['plugins']
 PLUGINS = ["render_math", "tag_cloud", "better_codeblock_line_numbering", "pelican-encrypt-content", "tipue_search", "pelican-toc"]
